cdea9111fd0be7067f24b938fb163620fb056200
Shivam Garg <gargshivam34has@gmail.com>
2022-07-21 15:11:13 +0530

    commit-21072022

--- delete_from_array.py
+++ delete_from_array.py
@@ -1,1 +1,11 @@
-print("Hello World")
+a=[1,2,3,4,5]
+e=2
+b=[]
+for i in range(len(a)):
+    if(a[i] != e):
+        b.append(a[i])
+
+if b==a:
+    print("element not found.")
+else:
+    print("element deleted. array :",b)
